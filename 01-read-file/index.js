a757c88b70a67e83039497bbfd6cc773c660f9f5
Konstantin Karpov <karpovkottt@gmail.com>
2023-05-05 16:14:13 +1000

    fix: fix eslint errors

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,5 +4,5 @@ let readStream = fs.createReadStream(__dirname + '/text.txt');
 readStream.setEncoding('utf-8');
 
 readStream.on('data', (chunk) => {
-    console.log(chunk);
-})
+  console.log(chunk);
+});
