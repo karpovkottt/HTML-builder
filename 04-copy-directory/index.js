15f9e0f56b657a6bf5f05524a3bd1b9c18257dcd
Konstantin Karpov <karpovkottt@gmail.com>
2023-05-07 20:14:31 +1000

    feat: implement function copies the files from folder to another folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs/promises');
 
+const pathFiles = path.join(__dirname, 'files');
+const pathFilesCopy = path.join(__dirname, 'files-copy');
+
+async function copyDirectory() {
+
+  await fsPromises.rm(path.join(__dirname, 'files-copy'), {recursive: true, force: true});
+
+  async function makeDirectory() {
+    fs.mkdir(pathFilesCopy, {recursive: true}, (err) => {
+      if (err) throw err;
+    });
+  }
+
+  makeDirectory().then (
+    fs.readdir(pathFiles, (err, files) => {
+      if (err) throw err;
+
+      files.forEach(file => {
+        const filePath = path.join(pathFiles, file);
+        const fileCopyPath = path.join(pathFilesCopy, file);
+        fs.copyFile(filePath, fileCopyPath, (err) => {
+          if (err) throw err;
+          console.log(`${file} **скопирован в папку files-copy`);
+        });
+      });
+    })
+  );
+}
+
+copyDirectory();
